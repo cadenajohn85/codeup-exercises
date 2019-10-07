096f1eda32622ad31c8c587c76686fc6970599f6
John Cadena <cadena.john.85@gmail.com>
2019-10-07 10:52:46 -0500

    refactor code to incorporate java-faker library and to process input in a separate method

--- src/main/java/Practice.java
+++ src/main/java/Practice.java
@@ -1,3 +1,4 @@
+import com.github.javafaker.Faker;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Scanner;
@@ -7,17 +8,37 @@ public class Practice {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+        Faker faker = new Faker();
 
-        System.out.println("Enter Something:");
-        String userInput = input.nextLine();
-        System.out.println("You entered: \"" + userInput + "\"");
-        if (StringUtils.isNumeric(userInput)) {
-            System.out.println("\"" + userInput + "\" is a number.");
-        } else {
-            System.out.println("\"" + userInput + "\" is not a (whole) number.");
+//        System.out.println("Enter Something:");
+//        String userInput = input.nextLine();
+
+//        processInput(userInput);
+
+//        String fake1 = faker.gameOfThrones().dragon();
+//        String fake2 = faker.gameOfThrones().quote();
+
+//        processInput(fake2);
+
+        String fakeInput;
+
+        for (int i = 0; i < 5; ++i) {
+            fakeInput = faker.gameOfThrones().quote();
+            processInput(fakeInput);
+            System.out.println("");
         }
-        System.out.println("Flipped case: " + StringUtils.swapCase(userInput));
-        System.out.println("Reversed: " + StringUtils.reverse(userInput));
 
+
+    }
+
+    public static void processInput(String someString) {
+        System.out.println("You entered: \"" + someString + "\"");
+        if (StringUtils.isNumeric(someString)) {
+            System.out.println("\"" + someString + "\" is a number.");
+        } else {
+            System.out.println("\"" + someString + "\" is not a (whole) number.");
+        }
+        System.out.println("Flipped case: " + StringUtils.swapCase(someString));
+        System.out.println("Reversed: " + StringUtils.reverse(someString));
     }
 }
